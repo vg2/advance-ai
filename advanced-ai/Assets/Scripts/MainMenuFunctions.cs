88eddf0335e5870dae57f36741029c0c9763bc11
Anthony <201580508@student.uj.ac.za>
2018-10-02 09:43:34 +0200

    Added functionality to start and exit buttons

---
+++ advanced-ai/Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+    public void StartSim()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitSim() 
+    {
+
+    }
+}
