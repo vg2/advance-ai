3754af383846d9234afc9fb32c87eb604878389a
Vishen Gounden <vgounden@silica.net>
2018-10-03 22:55:12 +0200

    initial main controller structure

---
+++ advanced-ai/Assets/Scripts/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MainController : MonoBehaviour
+    {
+        private readonly Movement _movement;
+        private readonly global::Evolution _evolution;
+        private List<OrigamiRobot> _robots;
+
+        public MainController()
+        {
+            _movement = new Movement();
+            _evolution = new global::Evolution();
+        }
+
+        public void StartGame()
+        {
+            _robots = new List<OrigamiRobot>(); // todo: replace with call to Robot Origin to generate robots
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
