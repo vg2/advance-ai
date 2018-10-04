1986b08890dd53101505920a4b2e23ef6c4630ad
philezwini <ssithungu@gmail.com>
2018-10-04 19:41:44 +0200

    Initialize() and GetLinks() function done.

--- advanced-ai/Assets/Scripts/TileScript.cs
+++ advanced-ai/Assets/Scripts/TileScript.cs
@@ -18,17 +18,42 @@ public class TileScript : MonoBehaviour
 
     GameObject[] upSide;
     GameObject[] downSide;
+    List<OrigamiRobot> robots;
 
     // Use this for initialization
     void Start()
     {
+        robots = new List<OrigamiRobot>();
         upSide = GameObject.FindGameObjectsWithTag("UpTag");
         downSide = GameObject.FindGameObjectsWithTag("DownTag");
 
+        Debug.Log("Creating upSide robots...");
         for (int i = 0; i < upSide.Length; i++)
         {
             OrigamiRobot robot = new OrigamiRobot(0, 10, upSide[i].transform);
-            Triangle[] robotParts = robot.getBody();
+            robots.Add(robot);
+        }
+
+        Debug.Log("Creating downSide robots...");
+        for (int i = 0; i < downSide.Length; i++)
+        {
+            OrigamiRobot robot = new OrigamiRobot(0, 10, downSide[i].transform);
+            robots.Add(robot);
+        }
+
+        Debug.Log("Total number of robots: " + robots.Count);
+
+        Dictionary<Triangle, List<Triangle>> links = robots[0].GetLinks();
+        foreach(KeyValuePair<Triangle, List<Triangle>> kv in links)
+        {
+            Debug.Log("---------------------------------------Triangle--------------------------------------------");
+            Debug.Log(kv.Key.GetVertexA() + " - " + kv.Key.GetVertexB() + " - " + kv.Key.GetVertexC());
+
+            Debug.Log("---------------------------------------Neighbours--------------------------------------------");
+            foreach (Triangle t in kv.Value)
+            {
+                Debug.Log(t.GetVertexA() + " - " + t.GetVertexB() + " - " + t.GetVertexC());
+            }
         }
     }
 }
--- advanced-ai/Assets/Scripts/Triangle.cs
+++ advanced-ai/Assets/Scripts/Triangle.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System;
 
 public class Triangle
 {
@@ -9,40 +11,157 @@ public class Triangle
     private Vector3 vertexA;
     private Vector3 vertexB;
     private Vector3 vertexC;
+    private List<Triangle> neighbours;
+    private List<int> vacantSides;
 
     public Triangle(Vector3 vertexA, Vector3 vertexB, Vector3 vertexC)
     {
         this.vertexA = vertexA;
         this.vertexB = vertexB;
         this.vertexC = vertexC;
+
+        neighbours = new List<Triangle>(3);
+        vacantSides = new List<int>(3)
+        {
+            1,
+            2,
+            3
+        };
     }
 
-    public Vector3 getVertexA()
+    public bool LinkNeighbour(Triangle n, int sideToUse)
+    {
+        if (neighbours.Contains(n))
+            return false;
+
+        if (vacantSides.Count == 0)
+            return false;
+
+        if (vacantSides.Contains(1))
+        {
+            switch (sideToUse)
+            {
+                case 1:
+                    n.SetVertexA(vertexA);
+                    n.SetVertexB(vertexB);
+                    break;
+                case 2:
+                    n.SetVertexA(vertexA);
+                    n.SetVertexC(vertexB);
+                    break;
+                case 3:
+                    n.SetVertexB(vertexA);
+                    n.SetVertexC(vertexB);
+                    break;
+            }
+            vacantSides.Remove(1);
+        }
+        else if(vacantSides.Contains(2))
+        {
+            switch (sideToUse)
+            {
+                case 1:
+                    n.SetVertexA(vertexA);
+                    n.SetVertexB(vertexC);
+                    break;
+                case 2:
+                    n.SetVertexA(vertexA);
+                    n.SetVertexC(vertexC);
+                    break;
+                case 3:
+                    n.SetVertexB(vertexA);
+                    n.SetVertexC(vertexC);
+                    break;
+            }
+            vacantSides.Remove(2);
+        }
+        else if (vacantSides.Contains(3))
+        {
+            switch (sideToUse)
+            {
+                case 1:
+                    n.SetVertexA(vertexB);
+                    n.SetVertexB(vertexC);
+                    break;
+                case 2:
+                    n.SetVertexA(vertexB);
+                    n.SetVertexC(vertexC);
+                    break;
+                case 3:
+                    n.SetVertexB(vertexB);
+                    n.SetVertexC(vertexC);
+                    break;
+            }
+            vacantSides.Remove(3);
+        }
+
+        n.GetNeighbours().Add(this);
+        n.vacantSides.Remove(sideToUse); //We have to remove the side we used from the new triangle's vacant sides.
+        neighbours.Add(n);
+        return true;
+    }
+
+    private int[] RandSides()
+    {
+        int[] sides = new int[2];
+        int r = UnityEngine.Random.Range(0, 3) + 1;
+        sides[0] = r; //We are basically picking a random side between 1, 2 and 3.
+        while(sides[0] == r)
+        {
+            r = UnityEngine.Random.Range(0, 3) + 1;
+        }
+        sides[1] = r; //We have found our second side.
+        return sides;
+    }
+
+    public List<int> GetVacantSides()
+    {
+        return vacantSides;
+    }
+
+    public List<Vector3> GetVertices()
+    {
+        List<Vector3> vertices = new List<Vector3>
+        {
+            vertexA,
+            vertexB,
+            vertexC
+        };
+
+        return vertices;
+    }
+
+    public List<Triangle> GetNeighbours()
+    {
+        return neighbours;
+    }
+
+    public Vector3 GetVertexA()
     {
         return vertexA;
     }
 
-    public Vector3 getVertexB()
+    public Vector3 GetVertexB()
     {
         return vertexB;
     }
 
-    public Vector3 getVertexC()
+    public Vector3 GetVertexC()
     {
         return vertexC;
     }
 
-    public void setVertexA(Vector3 vertexA)
+    public void SetVertexA(Vector3 vertexA)
     {
         this.vertexA = vertexA;
     }
 
-    public void setVertexB(Vector3 vertexB)
+    public void SetVertexB(Vector3 vertexB)
     {
         this.vertexB = vertexB;
     }
 
-    public void setVertexC(Vector3 vertexC)
+    public void SetVertexC(Vector3 vertexC)
     {
         this.vertexC = vertexC;
     }
