5d4c29f939dc73036413772a26d56b78031ba6b8
RENDANI MARTIN MESE <martinrendani@gmail.com>
2018-10-20 11:20:20 +0200

    dynamic tiles progress thus far

---
+++ advanced-ai/Assets/Scripts/DynamicTiles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicTiles : MonoBehaviour {
+
+
+
+   public GameObject[] hexagons;
+    // Use this for initialization
+    void Start () {
+            hexagons = GameObject.FindGameObjectsWithTag("DownTag");
+
+        if (hexagons != null)
+        {
+            foreach(GameObject hexagon in hexagons)
+            {
+                hexagon.SetActive(false);
+            }
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
