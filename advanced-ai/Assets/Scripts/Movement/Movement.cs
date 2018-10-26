fb60dd485edd10db41183eca586164ad8e22172d
philezwini <ssithungu@gmail.com>
2018-10-26 17:50:44 +0200

    Added DetermineMovement() function that uses the decision tree to move the robots around the field.

--- advanced-ai/Assets/Scripts/Movement/Movement.cs
+++ advanced-ai/Assets/Scripts/Movement/Movement.cs
@@ -1,10 +1,9 @@
 using Assets.Scripts;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement {
 
     /*
      * Author: Lance Govender (LG), Sihle Sithungu (SS), Fortune Chidzikwe (FC)
@@ -12,6 +11,13 @@ public class Movement : MonoBehaviour {
      * Behavour Parameters : Aggression - biases the bots to attack over run
     */
     private int Aggression;
+    DecisionTree movementStrategy; //Sequence of steps that the robot follows to make a movement decision.
+    List<OrigamiRobot> robots;
+    public Movement(int treeDepth)
+    {
+        //Instantiate the three with the desired depth.
+        movementStrategy = new DecisionTree(treeDepth);
+    }
 
     private int RandomNumber(int Min, int Max)
     {
@@ -49,8 +55,19 @@ public class Movement : MonoBehaviour {
         }
 
         //-- Use Decision Tree to move robots --//
-        DecisionTree tree = new DecisionTree(2);
-        Debug.Log(tree.ToString());
+        robots = new List<OrigamiRobot>();
+        robots.AddRange(teamA.GetRobots());
+        robots.AddRange(teamB.GetRobots());
+
+        foreach(OrigamiRobot r in teamA.GetRobots())
+        {
+            DetermineMove(r);
+        }
+
+        foreach (OrigamiRobot r in teamB.GetRobots())
+        {
+            DetermineMove(r);
+        }
         // ------------------------------------------------------------------------------------------ // End
 
         // ---------------------------------------- FC ----------------------------------------------
@@ -71,6 +88,82 @@ public class Movement : MonoBehaviour {
         //    }
         //}
 
+    }
+
+    private void DetermineMove(OrigamiRobot r)
+    {
+        DecisionTree.DTNode currentNode = movementStrategy.GetRoot();
+        while(currentNode.GetDecision() == DecisionTree.Decision.None)
+        {
+            if (IsStateOfRobot(currentNode.GetState(), r))
+            {
+                //The answer is yes, move to left child.
+                currentNode = currentNode.GetLeftChild();
+            }
+            else
+            {
+                //The answer is no, move to right child.
+                currentNode = currentNode.GetRightChild();
+            }
+        }
+
+        //We have finally reached a decision node.
+        ExecuteMove(r);
+    }
+
+    private bool IsStateOfRobot(DecisionTree.State state, OrigamiRobot r)
+    {
+        if(state == DecisionTree.State.InCollision)
+        {
+            foreach (OrigamiRobot rc in robots)
+            {
+                if(!rc.Equals(r) && rc.GetRobotTile().Equals(r.GetRobotTile()))
+                {
+                    //The two robots are in the same tile.
+                    return true;
+                }
+            }
+        }
+
+        if (state == DecisionTree.State.InForeignColony)
+        {
+            if (r.GetTeam().GetID() == "1")
+            {
+                //Robot's colony is UpTag.
+                return r.GetRobotTile().CompareTag("DownTag");
+            }
+
+            if (r.GetTeam().GetID() == "2")
+            {
+                //Robot's colony is DownTag.
+                return r.GetRobotTile().CompareTag("UpTag");
+            }
+        }
+
+        if (state == DecisionTree.State.InOwnColony)
+        {
+            if (r.GetTeam().GetID() == "1")
+            {
+                //Robot's colony is UpTag.
+                return r.GetRobotTile().CompareTag("UpTag");
+            }
+
+            if (r.GetTeam().GetID() == "2")
+            {
+                //Robot's colony is DownTag.
+                return r.GetRobotTile().CompareTag("DownTag");
+            }
+        }
+
+        throw new Exception("Robot is in an invalid state");
+    }
+
+    /*
+     * Part of the "update positions" sections for FC
+     */
+    private void ExecuteMove(OrigamiRobot r)
+    {
+        
     }
 
     // ---------------------------------------- LG ----------------------------------------------
@@ -112,13 +205,13 @@ public class Movement : MonoBehaviour {
         if (team == 'a')
         {
             // generate detectors for team a
-            CreateDetector(teamA);
+           // CreateDetector(teamA);
         }
         else 
         if(team == 'b')
         {
             // generate detectors for team b
-            CreateDetector(teamB);
+          //  CreateDetector(teamB);
 
         }
     }
