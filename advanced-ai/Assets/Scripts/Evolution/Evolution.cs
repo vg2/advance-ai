4ac58b7b1f6d4118665c3cbe144ed9e953ef8fc3
Vishen Gounden <vgounden@silica.net>
2018-10-28 21:10:29 +0200

    update default clonal selection to use string matching

--- advanced-ai/Assets/Scripts/Evolution/Evolution.cs
+++ advanced-ai/Assets/Scripts/Evolution/Evolution.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Assets.Scripts;
 using Assets.Scripts.Evolution.ClonalSelection;
+using Assets.Scripts.Evolution.StringMatching;
 
 public class Evolution
 {
@@ -9,12 +10,12 @@ public class Evolution
 
     public Evolution()
     {
-        _clonalSelection = ClonalSelectionAlgorithms.DefaultClonalSelection(new ClonalSelectionConfiguration(
+        _clonalSelection = ClonalSelectionAlgorithms.StringMatchingClonalSelection(new ClonalSelectionConfiguration(
             affinityThreshold:1,
             selectionLimit:10,
             defaultCloneSize:100,
             mutationProbabilityFactor:10
-        ));
+        ), new HammingDistanceRule());
     }
 
     public Team Evolve(Team losingTeamToEvolve, Team otherTeam)
